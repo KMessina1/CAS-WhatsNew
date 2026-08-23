63482c2d74530a762ca2e5ed24c0d958555fcb69
Kevin Messina <KMessina@CreativeApps.US>
2026-08-23 15:58:01 -0400

    Standardize iOS-first package platforms

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
      File: Package.swift
    Author: Kevin Messina
   Created: 8/22/26
- Modified: 08/22/2026 06:25 PM EDT
-  Version: 1
+ Modified: 08/23/2026 03:50 PM EDT
+  Version: 2
    Source: CODEX: (GPT-5) 🤖AI Code a portion or all of this code.
  
 ©2026 Creative App Solutions, LLC. - All Rights Reserved.
@@ -18,7 +18,8 @@ let package = Package(
     name: "CAS-WhatsNew",
     platforms: [
         .iOS(.v26),
-        .macCatalyst(.v26)
+        .macCatalyst(.v26),
+        .macOS(.v26)
     ],
     products: [
         .library(
